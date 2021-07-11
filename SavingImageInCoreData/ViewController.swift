f6179939f68350f13ef4cf2717b8c8a7976f2eb7
Minju Lee <prnbljk@gmail.com>
2021-07-12 01:45:07 +0900

    Initial Commit

---
+++ SavingImageInCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SavingImageInCoreData
+//
+//  Created by Minju Lee on 2021/07/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
